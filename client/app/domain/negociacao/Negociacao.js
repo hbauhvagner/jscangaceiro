c95cf70c20089f72520c6def4fff6464ee3a63bf
hbauhvagner <hbauhvagner@gmail.com>
2021-05-22 00:21:04 -0300

    Criando a classe negociacao

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,25 +1,7 @@
 class Negociacao {
-
-    constructor(_data, _quantidade, _valor) {
-
-        Object.assign(this, { _quantidade, _valor });
-        this._data = new Date(_data.getTime())
-        Object.freeze(this);
-    }
-
-    get volume() {
-        return this._quantidade * this._valor;
-    }
-
-    get data() {
-        return new Date(this._data.getTime());
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+  constructor() {
+    this.data = new Date();
+    this.quantidade = 1;
+    this.valor = 0.0;
+  }
 }
