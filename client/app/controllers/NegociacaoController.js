d42a47a2c65f9dd9a55ed3790ac8e17e3185c64f
hbauhvagner <hbauhvagner@gmail.com>
2021-05-28 23:38:42 -0300

    Associando métodos do controller às ações do usuário

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -3,6 +3,14 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    alert('Chamei ação no controller');
+    let $ = document.querySelector.bind(document);
+
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+
+    console.log(inputData.value);
+    console.log(parseInt(inputQuantidade.value));
+    console.log(parseFloat(inputValor.value));
   }
 }
